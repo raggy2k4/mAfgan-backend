45a28f9f847cb220be93ade5a395b24854f05a4a
SNTIS\Mariusz.Czerkies <czerkiesmariusz@gmail.com>
2023-03-06 22:01:53 +0100

    first commit

---
+++ src/main/java/com/example/mafgan/MAfganApplication.java
@@ -0,0 +1,13 @@
+package com.example.mafgan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MAfganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MAfganApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mafgan/MAfganApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mafgan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MAfganApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
