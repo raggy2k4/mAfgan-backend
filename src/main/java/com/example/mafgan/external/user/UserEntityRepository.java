ed00da0284ca94e0c8b1d588ade9ea4ae2d8de5f
marcin <raggy2k4@gmail.com>
2023-03-24 22:07:07 +0100

    fix/MAF-6 delete unused class

--- src/main/java/com/example/mafgan/external/user/UserEntityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mafgan.external.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
-}
